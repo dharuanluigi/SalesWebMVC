cca44327bbcfa4c57da50a243b45dba446647f87
Dharuan Emina <dharuan.emina.tc@take.net>
2021-09-02 23:50:47 -0300

    Added new required files

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+        
+        public double BaseSalary { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            this.Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            this.Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return this.Sales.Where(s => s.Date >= initial && s.Date <= final).Sum(s => s.Amount);
+        }
+    }
+}
